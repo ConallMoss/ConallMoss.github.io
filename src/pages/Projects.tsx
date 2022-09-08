af42e40448e01d82ef32d5145cc56aa04d5339c4
Conall Moss <conall.moss2003@gmail.com>
2022-09-08 18:26:46 +0100

    project text updated

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,7 +13,7 @@ export function Projects() {
             <img src="\imgs\cisco.jpg" alt="Cisco Company Logo" style={{height:"200px", width:"400px", padding:"10px", borderRadius:"75px"}}/>
             <div style={{padding:"20px"}}>
                 <h2>Cisco Micro-internship </h2>
-                <p>  </p>
+                <p> I worked at Cisco (technology and communications company) for one week as an intern, where I helped the company build an internal tool for converting between data structure types using Python. This was able to make improvements of 10-100x speeds to their previous system. </p>
             </div>
         </div>
 
@@ -23,10 +23,8 @@ export function Projects() {
             <img src="\imgs\checkersgamepic3.png" alt="Picture of Checkers game" style={{height:"300px", width:"500px", padding:"10px", borderRadius:"100px"}}/>
             <div style={{padding:"20px"}}>
                 <h2>Network Java Checkers Game</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
+                <p> This is an Online network based 2-played Checkers game I built in Java in order to better understand and implement some Object Oriented techniques I had recently learnt in my University course.
+                    I also used it to learn how to create a client-server networked system with sockets, as well as using a GUI in Java with userinputs.</p>
                 <a href="https://github.com/ConallMoss/Checkers" className="rect">
                     View on Github →
                 </a>
